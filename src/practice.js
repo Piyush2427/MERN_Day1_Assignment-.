d26ed0a73c57d7faaa938281cbee89ab86effa7e
Piyush2708 <piyushpandeynoida@gmail.com>
2026-04-15 11:15:14 +0530

    Update practice.js and public/index.html

--- src/practice.js
+++ src/practice.js
@@ -30,3 +30,73 @@ const studentProfile = {
 const number = [10, 25, 30, 15];
 const filtereed = number.filter((n) => n > 18);
 console.log(filtereed);
+
+// DOM
+const changeText = () => {
+  document.getElementById("btn").innerText = "Clicked!";
+};
+
+// Events
+let count = 0;
+
+const handleClick = () => {
+  count++;
+  console.log("Button clicked:", count);
+};
+
+// ES6 Features
+const dev = { name: "Piyush", course: "MERN" };
+
+// Destructuring
+const { name, course } = dev;
+console.log(name, course);
+
+// Spread
+const updatedDev = { ...dev, age: 20 };
+console.log(updatedDev);
+
+// OOP
+class Learner {
+  constructor(name, course) {
+    this.name = name;
+    this.course = course;
+  }
+
+  showDetails() {
+    console.log(`${this.name} is learning ${this.course}`);
+  }
+}
+
+const student1 = new Learner("Piyush", "MERN");
+student1.showDetails();
+
+// utils.js
+export const greetUser = (name) => {
+  return `Hello ${name}`;
+};
+
+import { greetUser } from "./utils";
+
+console.log(greetUser("Piyush"));
+
+// Promises
+const fetchData = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("Data fetched successfully");
+  }, 1000);
+});
+
+fetchData.then((res) => console.log(res));
+
+// Async/Await
+const getData = async () => {
+  const result = await fetchData;
+  console.log(result);
+};
+
+getData();
+
+// Forms
+const handleInput = (event) => {
+  console.log("Input value:", event.target.value);
+};
